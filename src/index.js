9bbeeaedc60370b2cb1543ac9936bdfffaa17746
zurdozarazaga <zurdozarazaga@gmail.com>
2022-03-12 16:59:27 -0300

    petition with graphql and apollo in console

--- src/index.js
+++ src/index.js
@@ -2,7 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './routes/App';
+import {ApolloClient, createHttpLink, gql} from '@apollo/client';
 
+import { setContext } from "@apollo/client/link/context";
+import { InMemoryCache } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+  
+});
+
+const authLink = setContext((_, {headers} ) => {
+  const token = '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';  
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+})
+// const link = authLink.concat(httpLink)
+
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+    });
+
+const query = gql`
+   query repositoryGitHub {
+          repository(owner: "facebook", name: "react") {
+            id
+            nameWithOwner
+            description
+            url
+          }
+        }`
+
+  client.query({query}).then(result => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,27 +24,27 @@ import {
 
 const Home = () => {
   // const { loading, error, data } = useQuery(REPO_GITHUB_QUERY);
-  useEffect(() => {
-    fetch('https://api.github.com/graphql', {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer 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`,
-        'Content-Type': 'application/json'
-        },
-      body: JSON.stringify({
-        query: `query {
-          repository(owner: "facebook", name: "react") {
-            id
-            nameWithOwner
-            description
-            url
-          }
-        }`
-      })
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
-  }, []);
+  // useEffect(() => {
+  //   fetch('https://api.github.com/graphql', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Authorization': `Bearer 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`,
+  //       'Content-Type': 'application/json'
+  //       },
+  //     body: JSON.stringify({
+  //       query: `query {
+  //         repository(owner: "facebook", name: "react") {
+  //           id
+  //           nameWithOwner
+  //           description
+  //           url
+  //         }
+  //       }`
+  //     })
+  //   })
+  //   .then(res => res.json())
+  //   .then(data => console.log(data))
+  // }, []);
   
   // if (loading) return <p>Loading...</p>;
   // if (error) return <p>Error </p>;
