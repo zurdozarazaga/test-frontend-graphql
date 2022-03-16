c05d2482c42eff6958223cb7e8eb5d5cc28f1f9f
zurdozarazaga <zurdozarazaga@gmail.com>
2022-03-15 21:18:42 -0300

    page home finished

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -6,11 +6,13 @@ const SEARCH_REPOSITORY = gql`
   query topRepos($query: String!) {
     search(first: 6, query: $query, type: REPOSITORY) {
       repositoryCount
-
       nodes {
         ... on Repository {
           nameWithOwner
           description
+          issues(states: OPEN) {
+            totalCount
+          }
         }
       }
     }
@@ -21,20 +23,21 @@ const List = () => {
   // useLazyQuery para manejar cuadno hacer la consulta
   const [getSearchRepository, result] = useLazyQuery(SEARCH_REPOSITORY);
   const [query, setQuery] = useState(null);
-  // const [valueInput, setValueInput] = useState(null);
-  // console.log(query);
-  // console.log(valueInput);
+  const [valueInput, setValueInput] = useState(null);
+  console.log(query);
+  console.log(valueInput);
   //manejador de click del search
   const handleSearch = (value) => {
     getSearchRepository({ variables: { query: value } });
   };
 
   // //manejador de cambio del input
-  // const handleValueInput = (e) => {
-  //   setValueInput(e.target.value);
-  // };
+  const handleValueInput = (e) => {
+    setValueInput(e.target.value);
+  };
 
   useEffect(() => {
+    console.log("result del effect", result);
     if (result.data) {
       setQuery(result.data.search.nodes);
     }
@@ -48,7 +51,9 @@ const List = () => {
             <h2 className="text-gray-600 font-semibold">
               Issues manager of the Github repository.
             </h2>
-            <span className="text-xs">All products item</span>
+            <div className=" mt-4">
+              <span className="text mt-3"></span>
+            </div>
           </div>
           <div className="flex items-center justify-between">
             <div className="flex bg-gray-50 items-center p-2 rounded-md">
@@ -70,18 +75,17 @@ const List = () => {
                 name=""
                 id=""
                 placeholder="search..."
-                // onChange={handleValueInput}
+                onChange={handleValueInput}
               ></input>
             </div>
             <div className="lg:ml-40 ml-10 space-x-8">
               <button
                 className="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer"
-                onClick={() => handleSearch('react')}
+                onClick={() => {
+                  handleSearch(valueInput);
+                }}
               >
                 Buscar
-              </button>
-              <button className="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">
-                Create
               </button>
             </div>
           </div>
@@ -99,52 +103,45 @@ const List = () => {
                       description
                     </th>
                     <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                      Created at
+                      Issues Open
                     </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                      QRT
-                    </th>
+
                     <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                       Status
                     </th>
                   </tr>
                 </thead>
                 <tbody>
-                  {query.map((repo) => (
-                    <tr>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <div className="flex items-center">
-                          <div className="ml-3">
-                            <p className="text-gray-900 whitespace-no-wrap">
-                              {repo.nameWithOwner}
-                            </p>
+                  {query &&
+                    query.map((repo) => (
+                      <tr>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          <div className="flex items-center">
+                            <div className="ml-3">
+                              <p className="text-gray-900 whitespace-no-wrap">
+                                {repo.nameWithOwner}
+                              </p>
+                            </div>
                           </div>
-                        </div>
-                      </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <p className="text-gray-900 whitespace-no-wrap">
-                          {repo.description}
-                        </p>
-                      </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <p className="text-gray-900 whitespace-no-wrap">
-                          Jan 21, 2020
-                        </p>
-                      </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <p className="text-gray-900 whitespace-no-wrap">43</p>
-                      </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                          <span
-                            aria-hidden
-                            className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-                          ></span>
-                          <span className="relative">Activo</span>
-                        </span>
-                      </td>
-                    </tr>
-                  ))}
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          <p className="text-gray-900 whitespace-no-wrap">
+                            {repo.description}
+                          </p>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          <p className="text-gray-900 whitespace-no-wrap">
+                            {repo.issues.totalCount}
+                          </p>
+                        </td>
+
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                          <button className="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">
+                            Buscar
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
                 </tbody>
               </table>
             </div>
